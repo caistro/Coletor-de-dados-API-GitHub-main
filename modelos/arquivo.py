3dcd39568dbbd82aa637c2107738319411013f61
cairo <cairoantonio9988@hotmail.com>
2026-02-26 15:08:16 -0300

    teste de implantação do csv

--- modelos/arquivo.py
+++ modelos/arquivo.py
@@ -25,14 +25,24 @@ class FileStorage:
     
     '''Ao tentar implantar o relatorio com CSV ele sai quebrado. Creio que devido a forma como está o aninhamento do dicionário'''
     # def salvar_csv(report: dict, username: str, output: str) -> csv:
-    #     pasta_saida = Path(output)
-    #     pasta_saida.mkdir(exist_ok=True, parents=True)
-    #     nome_do_arquivo = f'{pasta_saida}/report_{username}.csv'
-    #     cabecalho = list()
-    #     with open(nome_do_arquivo, 'w', newline='', encoding='utf-8') as relatorio:
-    #         writer = csv.DictWriter(relatorio, fieldnames=cabecalho)
-    #         writer.writeheader()
-    #         writer.writerows(report.values())
+        #     pasta_saida = Path(output)
+        #     pasta_saida.mkdir(exist_ok=True, parents=True)
+        #     nome_do_arquivo = f'{pasta_saida}/report_{username}.csv'
+
+        #     chave_principal = list()
+        #     chave_aninhada = list()
+
+        #     for i in report:
+        #         chave_principal.append(i)
+        #         if type(report.get(i,{})) == dict:    
+        #             chave_aninhada += report.get(i,{}).keys()
+        #     print(chave_principal)
+        #     print(chave_aninhada)
+
+        #     with open(nome_do_arquivo, 'w', newline='', encoding='utf-8') as relatorio:
+        #         writer = csv.DictWriter(relatorio, fieldnames=cabecalho)
+        #         writer.writeheader()
+        #         writer.writerows(report.values())
 
 
-            
+                
